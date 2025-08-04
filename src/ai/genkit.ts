cfc0c02f2dd0e640865dc3a26e0c664b938569b7
Leem 0nz <sd.aiart.arg@gmail.com>
2025-08-04 18:36:17 +0000

    2025-08-04T18:33:57Z [web]  POST /character-generator 500 in 481ms 2025-

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,15 +1,16 @@
+
 'use server';
 
 import { genkit } from 'genkit';
 import { googleAI } from '@genkit-ai/googleai';
-import { firebase } from '@genkit-ai/firebase';
+import { firebasePlugin } from '@genkit-ai/firebase';
 import { config } from 'dotenv';
 
 config({ path: '.env' });
 
 export const ai = genkit({
   plugins: [
-    firebase(),
+    firebasePlugin(),
     googleAI(),
   ],
 });
