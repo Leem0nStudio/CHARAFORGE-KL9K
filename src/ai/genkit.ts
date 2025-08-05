a414091bec8daf69d29949f0b8247de0104b3df6
Leem 0nz <sd.aiart.arg@gmail.com>
2025-08-05 00:23:34 +0000

    el archivo ya esta con las key correctas, pero aun sigue , le faltaran l

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,7 @@
 
 import { genkit } from 'genkit'; 
-import { config } from 'dotenv';
 import { enableFirebaseTelemetry } from '@genkit-ai/firebase';
 import { googleAI } from '@genkit-ai/googleai';
-
-config({ path: '.env' });
 
 export const ai = genkit({
   plugins: [
