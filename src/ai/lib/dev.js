57820a62c7ad56a4d8dbaa6b0ab096bfff120644
Leem 0nz <sd.aiart.arg@gmail.com>
2025-08-06 18:59:55 +0000

    hagamos una limpieza , dejemos todo prolijo por favor no rompas nada de

--- src/ai/lib/dev.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = require("dotenv");
-(0, dotenv_1.config)();
-require("@/ai/flows/generate-character-image.ts");
-require("@/ai/flows/generate-character-bio.ts");
-require("@/ai/flows/save-character.ts");
-//# sourceMappingURL=dev.js.map
