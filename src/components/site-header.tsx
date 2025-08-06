88c2bf1adea4531cdb5c8b015aef4d8445d79c56
Leem 0nz <sd.aiart.arg@gmail.com>
2025-08-06 18:22:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,3 +1,4 @@
+
 'use server';
 
 import Link from "next/link";
@@ -29,14 +30,14 @@ export async function SiteHeader() {
         <div className="flex flex-1 items-center justify-end space-x-2">
             <Link
               href="/datapacks"
-              className={cn(buttonVariants({ variant: "ghost" }), "hidden sm:flex")}
+              className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0 font-headline tracking-wider h-10 px-4 py-2 hover:bg-accent hover:text-accent-foreground hidden sm:flex"
             >
               <Package className="mr-2" />
               DataPacks
             </Link>
            <Link
               href="/character-generator"
-              className={cn(buttonVariants(), "bg-primary text-primary-foreground hover:bg-primary/90 hidden sm:flex font-headline")}
+              className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:h-4 [&_svg]:w-4 [&_svg]:shrink-0 font-headline tracking-wider bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 hidden sm:flex"
             >
               <Swords className="mr-2" />
               Create
