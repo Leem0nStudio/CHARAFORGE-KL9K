bf4f994aef800ef62ebddb9e0a056a2244becc73
Leem 0nz <sd.aiart.arg@gmail.com>
2025-08-07 15:11:52 +0000

    2025-08-07T15:09:23Z [web]  GET /datapacks 200 in 993ms 2025-08-07T15:09

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: '**',
-            },
-             {
-                protocol: 'http',
-                hostname: '**',
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
