843375fd64ac55a76d81dac8bb04913112c9fdc3
Leem 0nz <sd.aiart.arg@gmail.com>
2025-08-12 16:52:39 +0000

    on-recoverable-error.js:28 Uncaught Error: Invalid src prop (https://ima

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'huggingface.co',
-        port: '',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-        port: '',
-        pathname: '**',
-      }
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'image.civitai.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+                port: '',
+                pathname: '/**',
+            }
+        ],
+    },
 };
 
 export default nextConfig;
