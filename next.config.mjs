16c460c08cb7b28816ff144854cd44cea0461099
Leem 0nz <sd.aiart.arg@gmail.com>
2025-08-14 05:17:12 +0000

    Error: Invalid src prop (https://cdn.iconscout.com/icon/free/png-256/fre

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,25 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'image.civitai.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'placehold.co',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'storage.googleapis.com',
-                port: '',
-                pathname: '/**',
-            }
-        ],
-    },
-    experimental: {
-        allowedDevOrigins: ['9000-firebase-charaforgestudio-1754922151018.cluster-kc2r6y3mtba5mswcmol45orivs.cloudworkstations.dev'],
-        allowedDevOrigins: ['6000-firebase-charaforgestudio-1754922151018.cluster-kc2r6y3mtba5mswcmol45orivs.cloudworkstations.dev'],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.openai.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.iconscout.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
