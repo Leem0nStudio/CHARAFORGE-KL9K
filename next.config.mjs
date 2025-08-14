49d47dfb9b0096ff1f274819f4c4d9b2ccad3a0a
Leem 0nz <sd.aiart.arg@gmail.com>
2025-08-14 16:22:19 +0000

    Error: Invalid src prop (https://image.civitai.com/xG1nkqKTMzGDvpLrqFT7W

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'storage.googleapis.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.openai.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.iconscout.com',
-      },
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'placehold.co',
+            },
+            {
+                protocol: 'https',
+                hostname: 'storage.googleapis.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.openai.com',
+            },
+             {
+                protocol: 'https',
+                hostname: 'cdn.iconscout.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'image.civitai.com',
+            }
+        ],
+    },
 };
 
 export default nextConfig;
