24ee79c9a1891669668a9ad226d5a789597e095b
Leem 0nz <sd.aiart.arg@gmail.com>
2025-08-14 17:41:47 +0000

    Error: Invalid src prop (https://placehold.co/600x600.png) on `next/imag

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,38 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'storage.googleapis.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'image.civitai.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'upload.wikimedia.org',
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.civitai.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.openai.com',
+      },
+       {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+       {
+        protocol: 'https',
+        hostname: 'cdn.iconscout.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
