c50af9b6f1cad4693ed5f38e8c58794b75cd36a3
Leem 0nz <sd.aiart.arg@gmail.com>
2025-08-23 17:21:00 +0000

    ReferenceError: useFieldArray is not defined     at EditDetailsTab (webp

--- src/app/characters/[id]/edit/edit-details-tab.tsx
+++ src/app/characters/[id]/edit/edit-details-tab.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import { useTransition, useEffect, useState } from 'react';
-import { useForm, Controller } from 'react-hook-form';
+import { useForm, Controller, useFieldArray } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import type { Character, TimelineEvent } from '@/types/character';
