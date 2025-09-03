38dd36dc195670eb832fac904ef68048cad5cbc6
Leem 0nz <sd.aiart.arg@gmail.com>
2025-09-03 00:03:23 +0000

    Tenemos estos problemas

--- src/components/mobile-bottom-nav.tsx
+++ src/components/mobile-bottom-nav.tsx
@@ -3,8 +3,8 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { cn } from '@/lib/utils';
-import { useAuth } from '@/hooks/use-auth';
-import { mainNavItems } from '@/lib/app-config';
+import { useAuth } from '../hooks/use-auth';
+import { mainNavItems } from '../lib/app-config';
 
 export function MobileBottomNav() {
   const pathname = usePathname();
